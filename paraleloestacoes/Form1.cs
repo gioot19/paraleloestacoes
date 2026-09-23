5892efc917a4b5422ff1d137b4abc1d0842ae832
Giovanna <giovannatimoteo2@gmail.com>
2026-09-22 21:36:48 -0300

    up

---
+++ paraleloestacoes/Form1.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace paraleloestacoes
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Rad_primeiro_CheckedChanged(object sender, EventArgs e)
+        {
+            Pic_primeiro.Image = Properties.Resources.images; // Busca a imagem do resources
+            Txt_primeiro.Text = "Bad"; // Muda o texto do TextBox
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Pic_primeiro_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Txt_primeiro_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
